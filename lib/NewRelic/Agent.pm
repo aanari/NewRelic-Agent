d12c721bdb45504acf91463b2d4767d33d048fb3
Ali Anari <ali@anari.me>
2015-04-21 08:17:25 +0000

    cleanup, added perlobject.map

--- lib/NewRelic/Agent.pm
+++ lib/NewRelic/Agent.pm
@@ -1,19 +1,10 @@
 package NewRelic::Agent;
-
-use 5.008008;
 use strict;
 use warnings;
 
-use XSLoader ();
-require Exporter;
+our $VERSION = '0.01';
 
-our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [qw(
-)]);
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our $VERSION = '0.001';
-XSLoader::load __PACKAGE__, $VERSION;
+require XSLoader;
+XSLoader::load(__PACKAGE__, $VERSION);
 
 1;
