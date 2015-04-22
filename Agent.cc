0477675a79f09bb6076a2bff1bebc26eca48837d
Ali Anari <ali@anari.me>
2015-04-22 23:35:48 +0000

    Implemented transaction methods

--- Agent.cc
+++ Agent.cc
@@ -40,20 +40,24 @@ void Agent::initialize() {
   );
 }
 
-long Agent::begin_transaction(
-  const char* name = NULL,
-  bool is_web      = true
-) {
-  long transaction_id = newrelic_transaction_begin();
-  if (name)
-    newrelic_transaction_set_name(transaction_id, name);
-  if (!is_web)
-    newrelic_transaction_set_type_other(transaction_id);
-  return transaction_id;
+long Agent::begin_transaction() {
+  return newrelic_transaction_begin();
+}
+
+int Agent::set_transaction_name(long transaction_id, const char* name) {
+  return newrelic_transaction_set_name(transaction_id, name);
+}
+
+int Agent::add_transaction_attribute(long transaction_id, const char* key, const char* value) {
+  return newrelic_transaction_add_attribute(transaction_id, key, value);
+}
+
+int Agent::set_transaction_type_other(long transaction_id) {
+  return newrelic_transaction_set_type_other(transaction_id);
 }
 
 int Agent::end_transaction(long transaction_id) {
-    return newrelic_transaction_end(transaction_id);
+  return newrelic_transaction_end(transaction_id);
 }
 
 const char* Agent::get_license_key() {
