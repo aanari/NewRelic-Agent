74c73b065d4322424a5b05cce7f9531a18781a3e
Ali Anari <ali@anari.me>
2015-04-25 17:17:52 +0000

    Some method refactoring

--- lib/Plack/Middleware/NewRelic.pm
+++ lib/Plack/Middleware/NewRelic.pm
@@ -4,12 +4,12 @@ use Method::Signatures;
 use NewRelic::Agent;
 use Plack::Request;
 use Plack::Util;
-use Plack::Util::Accessor qw(agent enabled license_key app_name);
+use Plack::Util::Accessor qw(agent license_key app_name);
 
 # ABSTRACT: Plack middleware for NewRelic APM instrumentation
 
 method prepare_app {
-    return unless $self->enabled && $self->license_key && $self->app_name;
+    return unless $self->license_key && $self->app_name;
     $self->agent(
         NewRelic::Agent->new(
             license_key => $self->license_key,
@@ -20,25 +20,13 @@ method prepare_app {
 }
 
 method call($env) {
-    my $txn_id;
-    if ($self->agent) {
-        $txn_id = $self->agent->begin_transaction;
-        my $req = Plack::Request->new($env);
-        $self->agent->set_transaction_request_url($txn_id, $req->request_uri);
-        my $method = $req->method;
-        my $path   = $req->path;
-        $self->agent->set_transaction_name($txn_id, "$method $path");
-        for my $key (qw/Accept Accept-Language User-Agent/) {
-            my $value = $req->header($key);
-            $self->agent->add_transaction_attribute($txn_id, $key, $value)
-                if $value;
-        }
-    }
+    $self->begin_transaction($env)
+        if $self->agent;
 
     my $res = $self->app->($env);
  
     if (ref($res) and 'ARRAY' eq ref($res)) {
-        $self->agent->end_transaction($txn_id) if $txn_id;
+        $self->end_transaction($env);
         return $res;
     }
  
@@ -47,13 +35,38 @@ method call($env) {
         func($res) {
             func($chunk) {
                 if (!defined $chunk) {
-                    $self->agent->end_transaction($txn_id) if $txn_id;
+                    $self->end_transaction($env);
                     return;
                 }
                 return $chunk;
             }
         }
     );
+}
+
+method begin_transaction(HashRef $env) {
+    # Begin the transaction
+    my $txn_id = $self->agent->begin_transaction;
+    return unless $txn_id >= 0;
+    my $req = Plack::Request->new($env);
+    $env->{TRANSACTION_ID} = $txn_id;
+
+    # Populate transaction data
+    $self->agent->set_transaction_request_url($txn_id, $req->request_uri);
+    my $method = $req->method;
+    my $path   = $req->path;
+    $self->agent->set_transaction_name($txn_id, "$method $path");
+    for my $key (qw/Accept Accept-Language User-Agent/) {
+        my $value = $req->header($key);
+        $self->agent->add_transaction_attribute($txn_id, $key, $value)
+            if $value;
+    }
+}
+
+method end_transaction(HashRef $env) {
+    if (my $txn_id = $env->{TRANSACTION_ID}) {
+        $self->agent->end_transaction($txn_id);
+    }
 }
 
 1;
